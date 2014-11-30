b7dd8a9bda072004555c185bb3a119f22d530542
Dan Søndergaard <dan1990@gmail.com>
2014-11-30 12:54:14 +0100

    Switch to setuptools and add more metadata.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 
 from Cython.Build import cythonize
@@ -18,7 +18,12 @@ if USE_CYTHON:
 setup(
     name='tmhmm.py',
     version='1.0',
+    author='Dan Søndergaard',
+    author_email='das@birc.au.dk',
+    description='A transmembrane helix finder.',
+    url='https://github.com/dansondergaard/tmhmm.py/',
+    install_requires=['scikit-bio>=0.2', 'numpy>=1.9'],
     py_modules=['tmhmm'],
     ext_modules=extensions,
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()],
 )
