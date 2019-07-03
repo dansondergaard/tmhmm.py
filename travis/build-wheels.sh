5d799872388e55eb89343d0bd07f4c6b558d4756
Dan Søndergaard <dan1990@gmail.com>
2019-07-03 15:07:15 +0200

    Only build 3.5+

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e -x
 
+PYVERSIONS=cp35-cp35m cp36-cp36m cp37-cp37m
+
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYVER in $PYVERSIONS; do
+    PYBIN="/opt/python/$PYVER/bin"
     "${PYBIN}/pip" install -r /io/requirements-build.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -13,7 +16,8 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install tmhmm.py --no-index -f /io/wheelhouse
-    (cp /io/test.fa $HOME/; cd $HOME; tmhmm -f test.fa -p)
+for PYBIN in $PYVERSIONS; do
+    PYBIN="/opt/python/$PYVER/bin"
+    "${PYBIN}/pip" install tmhmm.py --user --no-index -f /io/wheelhouse
+    (cp /io/test.fa $HOME/; cd $HOME; /root/.local/bin/tmhmm -f test.fa -p)
 done
